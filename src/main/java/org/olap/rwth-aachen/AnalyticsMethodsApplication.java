85f0c2ee14ce1593eb1a659cb9e88bbf15026fd8
lechip <lechip@gmail.com>
2015-11-04 21:02:21 +0100

    -"First commit"

---
+++ src/main/java/org/olap/rwth-aachen/AnalyticsMethodsApplication.java
@@ -0,0 +1,12 @@
+package org.olap.rwth-aachen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsMethodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsMethodsApplication.class, args);
+    }
+}
