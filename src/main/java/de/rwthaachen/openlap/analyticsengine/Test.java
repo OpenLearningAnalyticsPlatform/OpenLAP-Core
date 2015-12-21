8303b387fdcdbe24bfe508f542edefb027220ece
Arham <arham.muslim@gmail.com>
2015-12-21 10:32:15 +0100

    just testing commit and other features

---
+++ src/main/java/de/rwthaachen/openlap/analyticsengine/Test.java
@@ -0,0 +1,11 @@
+package de.rwthaachen.openlap.analyticsengine;
+
+/**
+ * Created by Arham Muslim
+ * on 21-Dec-15.
+ */
+public class Test {
+    public String test(){
+        return "This is just a test";
+    }
+}
