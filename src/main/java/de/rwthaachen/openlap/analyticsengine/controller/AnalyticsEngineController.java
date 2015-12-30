c8875638523d918aa0bfff3335e8864263206636
Arham <arham.muslim@gmail.com>
2015-12-30 13:01:57 +0100

    basic skeleton of the project

---
+++ src/main/java/de/rwthaachen/openlap/analyticsengine/controller/AnalyticsEngineController.java
@@ -0,0 +1,14 @@
+package de.rwthaachen.openlap.analyticsengine.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by Arham Muslim
+ * on 30-Dec-15.
+ */
+@Controller
+public class AnalyticsEngineController {
+}
---
+++ src/main/java/de/rwthaachen/openlap/analyticsengine/model/Triad.java
@@ -0,0 +1,8 @@
+package de.rwthaachen.openlap.analyticsengine.model;
+
+/**
+ * Created by Arham Muslim
+ * on 30-Dec-15.
+ */
+public class Triad {
+}
